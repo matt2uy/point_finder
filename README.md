5b04fa9363915fadcaadd3dfb8d801cc6209ed60
unknown <matt2uy@gmail.com>
2017-11-03 14:09:01 -0400

    play a video

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Automatically edit tennis matches
 Dependencies:
 `pip install numpy`
 `pip install opencv_python`
+
+Download atp challenger video:
+website -> view-source -> find '.mp4' -> download it
--- demo.py
+++ demo.py
@@ -1,3 +1,5 @@
+# import image and convert it to greyscale
+'''
 import cv2
 
 image = cv2.imread("clouds.jpg")
@@ -5,4 +7,23 @@ gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow("Over the Clouds", image)
 cv2.imshow("Over the Clouds - gray", gray_image)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
+'''
+
+# play video from a file
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('test_match.mp4')
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(25) & 0xFF == ord('q'): # modify the parameter x in cv2.waitkey(x) to modify (playback speed?)
+        break
+
+cap.release()
 cv2.destroyAllWindows()
